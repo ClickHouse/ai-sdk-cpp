67f148c62f57759676ee871675fd5ae5b044d58f
Kaushik Iska <iska.kaushik@gmail.com>
2025-07-04 15:10:19 -0500

    remvoe

--- cmake/dependencies/nlohmann_json.cmake
+++
@@ -1,9 +0,0 @@
-# Fetch nlohmann/json
-CPMAddPackage(
-    NAME nlohmann_json
-    GITHUB_REPOSITORY nlohmann/json
-    VERSION 3.11.3
-    OPTIONS 
-        "JSON_BuildTests OFF"
-        "JSON_Install ON"
-)
